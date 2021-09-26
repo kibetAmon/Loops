6a9d2bcb8e09d04c875bd54719d045e58b6083c3
KibetAmon <kibetngetich805@gmail.com>
2021-09-26 23:49:17 +0300

    second commit

--- Question1.py
+++ Question1.py
@@ -1,13 +1,25 @@
 for x in range(1, 100):
     if x % 3 == 0 and x % 7 == 0:
-        print(x)
+        print("Numbers divisible by 7 and 3", x)
 
 for x in range(1, 100):
-    if x % 7 == 0 and x % 3 == 1:
-        print(x)
+    if x % 7 == 0 and x % 3 != 0:
+        print("Numbers divisible by 7 and not 3", x)
 
 
 for x in range(1, 100):
-    if x % 2 == 1 and x % 7 == 0 and x % 3 == 1:
-        print(x)
+    if x % 2 != 0 and x % 7 == 0 and x % 3 != 0:
+        print("Odd numbers divisible by 7 and not 3", x)
+
+for x in range(1, 100):
+    sum_of_digits = sum(int(digit) for digit in str(x))
+    if x % sum_of_digits == 0:
+        print("Numbers divisible by sum of its digits", x)
+
+for x in range(1, 100):
+    sum_of_digits = sum(int(digit) for digit in str(x))
+    if(pow(sum_of_digits, 2)) == x:
+        print("Numbers that are equal to the square of the sum of its digits", x)
+
+
 
