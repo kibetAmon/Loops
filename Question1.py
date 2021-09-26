7c3d87aac6f41463ddd3b38c430c8f94120d88e6
KibetAmon <kibetngetich805@gmail.com>
2021-09-26 23:22:05 +0300

    second commit

--- Question1.py
+++ Question1.py
@@ -1,16 +1,13 @@
 for x in range(1, 100):
-    if((x%3==0) & (x%7==0)):
+    if x % 3 == 0 and x % 7 == 0:
         print(x)
 
 for x in range(1, 100):
-    if((x%7==0)&(x%3==1)):
+    if x % 7 == 0 and x % 3 == 1:
         print(x)
 
-def isdivisible():
-    for x in range(1, 100):
-        if x % 2 == 1 and x % 7 == 0 and x % 3 == 1:
-            print(x)
 
-
-isdivisible()
+for x in range(1, 100):
+    if x % 2 == 1 and x % 7 == 0 and x % 3 == 1:
+        print(x)
 
